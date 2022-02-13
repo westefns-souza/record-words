ce1486d5b81ed5d8c1d8f3a9a299ca651b81a161
westefns-souza <westefns@outlook.com.br>
2022-02-12 21:29:08 -0300

    Exibição da listagem das palavras na tela inicial e o formulario de cadastro

--- app/src/main/java/com/westefns/recordswords/MainActivity.java
+++ app/src/main/java/com/westefns/recordswords/MainActivity.java
@@ -2,13 +2,49 @@ package com.westefns.recordswords;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.westefns.recordswords.dao.RecordWordDao;
+import com.westefns.recordswords.model.RecordWord;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+    RecordWordDao recordWordDao;
+
+    FloatingActionButton fabAddNewRecordWord;
+    ListView lvRecordsWords;
+
+    List<RecordWord> listRecordWords = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        recordWordDao = new RecordWordDao(MainActivity.this);
+
+        fabAddNewRecordWord = findViewById(R.id.fabAddNewRecordWord);
+        lvRecordsWords = findViewById(R.id.lvRecordsWords);
+
+        fabAddNewRecordWord.setOnClickListener(v -> {
+            Intent it = new Intent(getApplicationContext(), NewRecordWordActivity.class);
+
+            startActivity(it);
+        });
+
+        listRecordWords = recordWordDao.getAllRecordsWord();
+
+        ArrayAdapter<RecordWord> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, listRecordWords);
+
+        lvRecordsWords.setAdapter(adapter);
     }
 }
---
+++ app/src/main/java/com/westefns/recordswords/NewRecordWordActivity.java
@@ -0,0 +1,62 @@
+package com.westefns.recordswords;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+
+import com.westefns.recordswords.dao.RecordWordDao;
+import com.westefns.recordswords.model.RecordWord;
+
+import java.util.List;
+
+public class NewRecordWordActivity extends AppCompatActivity {
+    RecordWordDao recordWordDao;
+
+    EditText etvWord;
+    EditText etvTranslation;
+    Spinner spinnerClassification;
+
+    Button btnSave;
+
+    private String[] classifications = { "verbo", "Substantivo", "Artigo" };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_record_word);
+        recordWordDao = new RecordWordDao(NewRecordWordActivity.this);
+
+        etvWord = findViewById(R.id.etvWord);
+        etvTranslation = findViewById(R.id.etvTranslation);
+        spinnerClassification = findViewById(R.id.spinnerClassification);
+        btnSave = findViewById(R.id.btnSave);
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(NewRecordWordActivity.this, android.R.layout.simple_spinner_item, classifications);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        spinnerClassification.setAdapter(adapter);
+
+        btnSave.setOnClickListener(v -> {
+            RecordWord recordWord = new RecordWord();
+            recordWord.setWord(etvWord.getText().toString());
+            recordWord.setTranslation(etvTranslation.getText().toString());
+            recordWord.setClassification(spinnerClassification.getSelectedItem().toString());
+
+            boolean recordWordSave = recordWordDao.create(recordWord);
+
+            if (recordWordSave) {
+                Intent it = new Intent(getApplicationContext(), MainActivity.class);
+
+                startActivity(it);
+            } else {
+              // Notificar erro
+            }
+        });
+    }
+}
