38be88741d5455aceb9b060eef78f6743cfdd793
westefns-souza <westefns@outlook.com.br>
2022-02-12 21:26:18 -0300

    Correções na DBHelper o código de criar a tabela estava errado

--- app/src/main/java/com/westefns/recordswords/util/DBHelper.java
+++ app/src/main/java/com/westefns/recordswords/util/DBHelper.java
@@ -1,6 +1,7 @@
 package com.westefns.recordswords.util;
 
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.util.Log;
@@ -10,9 +11,19 @@ import androidx.annotation.Nullable;
 public class DBHelper extends SQLiteOpenHelper {
 
     public static int VERSION = 1;
-    public static String DBNAME = "db_records_words";
+    public static String DBNAME = "db_records_words.db";
 
     public static String TABLE_WORDS = "words";
+
+    private static final String SQL_CREATE_TABLE_WORDS = "CREATE TABLE IF NOT EXISTS " + TABLE_WORDS
+                + " (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, "
+                + "word VARCHAR(50) NOT NULL, "
+                + "classification VARCHAR(50) NOT NULL, "
+                + "translation VARCHAR(50) NOT NULL, "
+                + "create_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL)";
+
+    private static final String SQL_DROP =
+            "DROP TABLE IF EXISTS " + TABLE_WORDS;
 
     public DBHelper(@Nullable Context context) {
         super(context, DBNAME, null, VERSION);
@@ -20,15 +31,8 @@ public class DBHelper extends SQLiteOpenHelper {
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-        String sql = "CREATE TABLE IF NOT EXISTS " + TABLE_WORDS
-                + "(id INTERGER PRIMARY KEY AUTOINCREMENT NOT NULL, "
-                + "word VARCAHR(50) NOT NULL, "
-                + "classification VARCAHR(50) NOT NULL, "
-                + "translation VARCAHR(50) NOT NULL, "
-                + "created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP);";
-
         try {
-            db.execSQL(sql);
+            db.execSQL(SQL_CREATE_TABLE_WORDS);
 
             Log.i("WORD DB", "Sucesso ao criar o banco de dados!");
         } catch (Exception e) {
@@ -38,10 +42,9 @@ public class DBHelper extends SQLiteOpenHelper {
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-        String sql = "DROP TABLE IF EXISTS" + TABLE_WORDS + ";";
-
         try {
-            db.execSQL(sql);
+            db.execSQL(SQL_DROP);
+            onCreate(db);
         } catch (Exception e) {
             Log.i("WORD DB", "Erro ao apagar as tabelas!");
         }
