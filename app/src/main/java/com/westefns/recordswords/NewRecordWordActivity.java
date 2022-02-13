e019cd19c58d50e619428bcd2696690932cd5351
westefns-souza <westefns@outlook.com.br>
2022-02-12 22:36:56 -0300

    Colocando o V de verbo maisculo

--- app/src/main/java/com/westefns/recordswords/NewRecordWordActivity.java
+++ app/src/main/java/com/westefns/recordswords/NewRecordWordActivity.java
@@ -24,7 +24,7 @@ public class NewRecordWordActivity extends AppCompatActivity {
 
     Button btnSave;
 
-    private String[] classifications = { "verbo", "Substantivo", "Artigo" };
+    private String[] classifications = { "Verbo", "Substantivo", "Artigo" };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
